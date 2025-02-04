bdca0eda0e53bf41bb992c7857f0119b9373225b
Adkdotdoge <101447807+Adkdotdoge@users.noreply.github.com>
2025-02-04 03:08:04 -0500

    new nav

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -7,21 +7,28 @@ import { RouterModule } from '@angular/router';
   selector: 'app-root',
   template: `
   <main>
-    <header class="brand-name">
-      <nav class="d-flex">
-      <div class="row g-2" style="width: 100%;">
-      <div class="col-sm-6 col-12">
-      <h2>Adamkizis.com</h2>
-      <h6>Web Development Portfolio</h6>
+
+    <header class="brand-name m-0 p-0">
+
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"><h3>Adam Kizis</h3>
+    <h6>Web Development Portfolio</h6>
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a href="https://adamkizis.myportfolio.com/services" class="nav-link btn primary m-1">Pre-media & Print</a>
+      <a href="https://adamkizis.com" class="nav-link btn primary m-1">adamkizis.com</a>
+      <a href="https://aetherbind.com" class="nav-link btn primary m-1">Agency</a>
       </div>
-      <div class="col-md-6 col-12">
-      <a href="https://adamkizis.myportfolio.com/services" class="btn primary m-1">Pre-media & Print</a>
-      <a href="https://adamkizis.com" class="btn primary m-1">adamkizis.com</a>
-      <a href="https://aetherbind.com" class="btn primary m-1">Agency</a>
-      </div>
-      </div>
-      </nav>
+    </div>
+  </div>
+</nav>
       </header>
+      
       <section class="content">
       <router-outlet></router-outlet>
       </section>
